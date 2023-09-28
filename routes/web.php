27d7b946463c5a5639d0e53a952141eeb9bee09c
HassanAldhi <Hassanaldhi@gmail.com>
2023-09-28 14:30:32 +0700

    push quiz 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 //route resource
-Route::resource('/', \App\Http\Controllers\PostController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/post', [PostController::class, 'index']);
